1b23045ac1423ab7e93ff3834fc02561e5479509
Manish Choudhary <manishchoudharyy7@gmail.com>
2026-04-11 10:54:02 +0530

    add favicon icons

--- app/layout.js
+++ app/layout.js
@@ -107,6 +107,18 @@ export const metadata = {
       "en-IN": "/",
     },
   },
+  icons: {
+    icon: [
+      { url: "/favicons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicons/favicon-48x48.png", sizes: "48x48", type: "image/png" },
+    ],
+    shortcut: "/favicons/favicon.ico",
+    apple: [
+      { url: "/favicons/apple-touch-icon.png" },
+      { url: "/favicons/apple-touch-icon-180x180.png", sizes: "180x180" },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
--- app/manifest.js
+++ app/manifest.js
@@ -17,6 +17,18 @@ export default function manifest() {
         type: "image/webp",
         purpose: "any maskable",
       },
+      {
+        src: "/favicons/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/favicons/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
     ],
   };
 }
